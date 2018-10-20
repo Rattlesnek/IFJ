eac3472791d08c4209ebb4ba567493ddded9f1a6
Lukas Valek <xvalek15@fit.vutbr.cz>
2018-10-20 23:47:33 +0200

    WORK IN PROGRESS

--- token.h
+++ token.h
@@ -67,8 +67,21 @@ struct token {
 ///                    FUNCTION DECLARATIONS                         ///
 ////////////////////////////////////////////////////////////////////////
 
+/**
+ * @brief Creates token and initializes it
+ * 
+ * @param name token name eg. "ID", "STR", "+", "==", "EOL", ...
+ * @param info either info or ptr to corresponding info-element in hashtable
+ * @return token_t* initialized token or NULL if not succesful
+ */
 token_t *tk_CreateToken(char *name, token_info_t info);
 
+
+/**
+ * @brief Destroys token
+ * 
+ * @param token token to be destroyed
+ */
 void tk_DestroyToken(token_t *token);
 
 
