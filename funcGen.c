fd66e3cf076ebbc946cdab88318a6e58c628709e
Adam Pankuch <adam.pankuch@gmail.com>
2018-11-20 19:47:33 +0100

    Generovanie definicii funkcii

--- funcGen.c
+++ funcGen.c
@@ -8,21 +8,22 @@
 #include "token.h"
 
 
-bool funcGen(stack_str_t *stack_str, elem_t *fun, dynamicArrParam_t *param_arr, int param_cnt)
+bool funcGen(stack_str_t *stack_str, elem_t *fun, dynamicArrParam_t *param_arr)
 {
     printf( "\n"
             "LABEL $%s\n" 
             "PUSHFRAME\n"
-            "DEFVAR LF@%retval\n"
-            "MOVE LF@%retval nil@nil\n", 
+            "DEFVAR LF@%%retval\n"
+            "MOVE LF@%%retval nil@nil\n", 
             fun->func.key
     );
     
     for (int i = 0; i < fun->func.n_params; i++)
         printf( "DEFVAR LF@%s\n"
-                "MOVE LF@%s LF@%1\n", 
+                "MOVE LF@%s LF@%%%d\n", 
                 param_arr->param_arr[i]->var.key,
-                param_arr->param_arr[i]->var.key
+                param_arr->param_arr[i]->var.key,
+                i+1
         );
 
     printf("\n");
