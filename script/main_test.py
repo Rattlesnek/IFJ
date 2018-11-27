85ec4ed14ec46bf277894268db9630a85a9defd6
Adam Pankuch <adam.pankuch@gmail.com>
2018-11-27 15:28:42 +0100

    Testovaci skript rozsireny o testovanie nespravnych vstupov

--- script/main_test.py
+++ script/main_test.py
@@ -6,6 +6,17 @@ import subprocess
 class BinariesNotFound(Exception): pass
 class TestFilesNotFound(Exception): pass
 
+errors = {
+    "SUCCESS" : 0,
+    "ERR_LEX" : 1,
+    "ERR_SYN" : 2,
+    "ERR_SEM_UNDEF" : 3,
+    "ERR_SEM_TYPE" : 4,
+    "ERR_SEM_FUNC" : 5,
+    "ERR_SEM_OTHER" : 6,
+    "ERR_ZERO_DIV" : 7,
+    "ERR_INTERNAL" : 99
+}
 
 def check_all_needed_files():
     if not (os.path.isfile('../parser') and os.path.isfile('./ic18int') and os.path.isfile('./ifj18.rb')):
@@ -27,17 +38,32 @@ def prepare_filenames(folder):
     return test_input, user_input, generate, inter_out, ruby_out, valgrind
 
 
+def return_call(names):
+    with open(names[0], "r") as fr:
+        first_line = fr.readline().strip()
+
+    add = ' false 0'
+    if first_line.startswith('# ERROR'):
+        err_name = first_line.replace('# ERROR', '').strip()
+        add = ' true ' + str(errors[err_name])
+
+    call = './test_run.sh'
+    for name in names:
+        call += ' ' + name
+    call += add
+
+    return call
+
+
 def run_test(folder):
     names = prepare_filenames(folder)
     if not (os.path.isfile(names[0]) and os.path.isfile(names[1])):
         raise TestFilesNotFound(folder)
-    call = './test_run.sh'
-    for name in names:
-        call += ' ' + name
+    
+    call = return_call(names)
     
     print(folder[6:], end=" ... ", flush=True)
     subprocess.call(call, shell=True)
-
 
 
 try:
@@ -51,4 +77,8 @@ except BinariesNotFound:
     sys.exit(1)
 except TestFilesNotFound as err:
     print('Test file test_input.rb or user_input.txt is missing in folder: ', err)
+    sys.exit(1)
+except KeyError as err:
+    print('Key:', err, 'not in known errors')
+    print('Error occurred in folder:', folder)
     sys.exit(1)
--- script/test_run.sh
+++ script/test_run.sh
@@ -4,9 +4,20 @@ NC='\033[0m'
 GREEN='\e[32m'
 DIM='\e[2m'
 
-run_compiler()
+SUCCESS=0
+ERR_LEX=1
+ERR_SYN=2
+ERR_SEM_UNDEF=3
+ERR_SEM_TYPE=4
+ERR_SEM_FUNC=5
+ERR_SEM_OTHER=6
+ERR_ZERO_DIV=7
+ERR_INTERNAL=99
+
+
+run_with_valgrind()
 {
-    valgrind --leak-check=yes --error-exitcode=20 --log-file="$3" ../parser <$1 >$2
+    valgrind --leak-check=yes --error-exitcode=20 --log-file="$3" ../parser <$1 >$2 2>$2
     if [[ "$?" == "20" ]]
     then
         printf "${RED}(X)${NC}: "
@@ -15,7 +26,7 @@ run_compiler()
     fi
 }
 
-if [[ $# -eq 6 ]] && [[ -f $1 ]] && [[ -f $2 ]]
+if [[ $# == 8 ]] && [[ -f $1 ]] && [[ -f $2 ]]
 then
     testing=$1
     user_input=$2
@@ -23,19 +34,43 @@ then
     inter_out=$4
     ruby_out=$5
     valgr=$6
+    err_test=$7
+    expected_err=$8
 
-    run_compiler $testing $generate $valgr
-    ./ic18int $generate <$user_input >$inter_out
+    run_with_valgrind $testing $generate $valgr
 
-    ruby -r ./ifj18.rb $testing <$user_input >$ruby_out
-    
-    diff $inter_out $ruby_out >/dev/null
-    if [[ $? == 0 ]] 
+    if [[ "$err_test" == "false" ]]
     then
-        printf "${GREEN}SUCCESS${NC}\n"
-    else
-        printf "${RED}FAIL${NC}\n"
+        ./ic18int $generate <$user_input >$inter_out
+
+        ruby -r ./ifj18.rb $testing <$user_input >$ruby_out
+        
+        diff $inter_out $ruby_out >/dev/null
+        if [[ $? == 0 ]] 
+        then
+            printf "${GREEN}SUCCESS${NC}\n"
+        else
+            printf "${RED}FAIL${NC}\n"
+        fi
+
+    elif [[ "$err_test" == "true" ]]
+    then
+        ../parser <$testing >$generate 2>/dev/null
+        return_code=$?
+        
+        if [[ $return_code == $SUCCESS ]]
+        then
+            ./ic18int $generate <$user_input >$inter_out
+            return_code=$?
+        fi
+
+        if [[ $return_code == "$expected_err" ]]
+        then
+            printf "${GREEN}SUCCESS (error same as expected)${NC}\n"
+        else
+            printf "${RED}FAIL (expected error: $expected_err got: $return_code) ${NC}\n"
+        fi
     fi
 else
-    echo "HELP: ./test.sh f_testing f_input f_generate f_interpret_out f_ruby_out f_valgrind"
+    echo "HELP: ./test.sh f_testing f_input f_generate f_interpret_out f_ruby_out f_valgrind {true, false} {0,1,2,3,4,5,6,7,99}"
 fi
