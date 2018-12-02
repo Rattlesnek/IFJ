cb3661e44f510cb33a96d1da5015e511ff792510
Jindrich Sestak <xsesta05@vutbr.cz>
2018-12-02 21:57:30 +0100

    dva testy na builtin funkce

---
+++ tests/tests/18_test_builtin_func/test_input.rb
@@ -0,0 +1,35 @@
+a = 98
+b = "JAMBALAJA"
+c = 4
+d = 25
+
+chr(a)
+chr c
+chr 42
+
+
+
+ord( b, c)
+ord b, 8
+ord "Jak se vede", 3
+ord "Jde to skvele" , c
+
+
+print b
+print "Nazdarek"
+
+q = inputi
+r = inputi()
+
+s = inputs
+t = inputs()
+
+u = inputf
+v = inputf()
+
+substr( b, c, d)
+substr "AHOJ svete" , 1, 55
+substr b, c, d
+substr b, c, 22
+substr b, 1, d
+substr b, 1, 22
---
+++ tests/tests/19_test_builtin_func/test_input.rb
@@ -0,0 +1,51 @@
+a = 98
+b = "JAMBALAJA"
+c = 4
+d = 25
+
+def ch(z)
+	a = 5
+	chr(a)
+	chr(z)
+	chr z
+	chr 42
+end
+
+def ordinal(q,r)
+	ord( q, r)
+	ord q, 8
+	ord "Jak se vede", 3
+	ord "Jde to skvele" , r
+end
+
+def printing(o)
+	print o
+	print "Nazdarek"
+end
+
+def inputting(p)
+	p = inputi
+	p = inputi()
+
+	p = inputs
+	p = inputs()
+
+	p = inputf
+	p = inputf()
+end
+
+def substring(s,b,e)
+	substr( s, b, e)
+	substr "AHOJ svete" , 1, 55
+	substr s, b, e
+	substr s, b, 22
+	substr s, 1, e
+	substr s, 1, 22
+end
+
+ch(a)
+ordinal(b,c)
+printing(b)
+inputting(a)
+substring(b,c,d)
+
